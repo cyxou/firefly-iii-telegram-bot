70b10f7749af9efc7a6272a9bb9960da9b0a7b2b
Alex Sukhov <cyxou@gmail.com>
2021-10-10 14:38:10 +0300

    Remove unused parse-mode dependency

--- src/types/MyContext.ts
+++ src/types/MyContext.ts
@@ -1,5 +1,4 @@
 import type { Context, SessionFlavor } from 'grammy'
 import { SessionData } from './SessionData'
-import type { ParseModeContext } from '@grammyjs/parse-mode'
 
-export type MyContext = Context & ParseModeContext & SessionFlavor<SessionData>
+export type MyContext = Context & SessionFlavor<SessionData>
