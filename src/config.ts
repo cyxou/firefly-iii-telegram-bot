e892215de958f4be6af3b249ad624a9b0ec4e16f
Alex Sukhov <cyxou@gmail.com>
2025-07-20 17:39:48 +0300

    feat: add basic authorization logic
    closes #17

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,15 @@ if (process.env.DEBUG) Debug.enable(process.env.DEBUG)
 dotenv.config()
 
 export default {
-  userId: parseInt(process.env.TG_USER_ID || '', 10),
+  // Logging of unauthorized users is enabled by default; disable if env var is set to 'true'
+  disableUnauthorizedUserLog: process.env.DISABLE_UNAUTHORIZED_USER_LOG === 'true',
+  allowedUserIds: process.env.ALLOWED_TG_USER_IDS
+    ? process.env.ALLOWED_TG_USER_IDS.split(',')
+        .map(id => id.trim())
+        .filter(Boolean)
+        .map(id => Number(id))
+        .filter(id => !isNaN(id))
+    : undefined,
   botToken: process.env.BOT_TOKEN || 'USE_YOUR_REAL_BOT_TOKEN',
   fireflyUrl: process.env.FIREFLY_URL || '',
   fireflyApiUrl: process.env.FIREFLY_API_URL || `${process.env.FIREFLY_URL}/api`,
